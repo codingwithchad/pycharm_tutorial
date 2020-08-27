d25b5d4e9c0b48e4c3d678516970c767959044c9
Chad H <chadg1980@gmail.com>
2020-08-27 13:39:10 -0700

    added location of player

--- main.py
+++ main.py
@@ -2,38 +2,50 @@ import pygame
 
 # Initialize pygame
 pygame.init()
-WHITE = (200, 200, 200)
-DISPLAY_WIDTH = 600
-DISPLAY_HEIGHT = 800
-# Create the screen (width, height)
-screen = pygame.display.set_mode((DISPLAY_HEIGHT, DISPLAY_WIDTH))
-# RGB values (red, green, blue)
-screen.fill((56, 25, 255))
 
+# COLORS RGB
+WHITE = (200, 200, 200)
+SKY_BLUE = (135, 206, 235)
+
+# Display size
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+# Create the screen (width, height)
+screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+# Player
+player_img = pygame.image.load("assets\\space_invaders_defense.png")
+player_x = DISPLAY_WIDTH / 2
+player_y = ((DISPLAY_HEIGHT / 4)*3)
+
+# Fill the screen with a color
+# Objects render in order, screen.fill should be first
+screen.fill(SKY_BLUE)
 # Title and Icon
 pygame.display.set_caption("Seagull Invaders")
 # icon By Freepik
-icon = pygame.image.load("assets\\bird.png")
+icon = pygame.image.load("assets\\bird.png") # Icons made by Freepik from www.flaticon.com
 pygame.display.set_icon(icon)
-
-# Player
-playerImg = pygame.image.load("assets\\space_invaders_defense.png")
-playerX = 370
-playerY = 480
-
-
-
 
 # Game loop
 running = True
+
+# Function to add the player to the screen
+def player0():
+    screen.blit(player_img, (player_x, player_y))
+
+
 while running:
 
+    # Keeps the loop from being infinite.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # Calls the player function
+    player0()
     pygame.display.update()
-    # <div>Icons made by Freepik from www.flaticon.com
+
 
 
 
