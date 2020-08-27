9c16633fc654f86e44873d8133eff6ec1b21efed
Chad H <chadg1980@gmail.com>
2020-08-27 10:21:24 -0700

    added icon. Changed name to bird invaders

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
 
-running = True
+# Title and Icon
+pygame.display.set_caption("Seagull Invaders")
+# icon By Freepik
+icon = pygame.image.load("assets\\bird.png")
+pygame.display.set_icon(icon)
 
 # Game loop
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
